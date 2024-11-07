e6d783a84707cb1084b344e4cae12258e05dbbc7
kinggora <jhha09@ajou.ac.kr>
2024-11-07 12:05:21 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/com/tms/sportlight/SportlightApplication.java
@@ -0,0 +1,13 @@
+package com.tms.sportlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tms/sportlight/SportlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.sportlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
