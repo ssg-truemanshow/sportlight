0ad501fee4246d2156bd08c099f48ff78952a40f
kinggora <jhha09@ajou.ac.kr>
2024-11-13 10:42:08 +0900

    🔧 웹 설정 파일 추가
    - 포맷터, CORS 설정

---
+++ src/main/java/com/tms/sportlight/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.tms.sportlight.config;
+
+import com.tms.sportlight.controller.formatter.IdConverter;
+import com.tms.sportlight.controller.formatter.LocalDateTimeFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new IdConverter());
+        registry.addFormatter(new LocalDateTimeFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+    }
+
+}
