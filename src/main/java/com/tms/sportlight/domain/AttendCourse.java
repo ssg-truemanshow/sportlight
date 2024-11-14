c92ad0f1f804a12cad63ec78ead0ad3171125178
KimJeongu <kjw101147@gmail.com>
2024-11-14 15:18:23 +0900

    ✨ 수강 엔티티 추가

---
+++ src/main/java/com/tms/sportlight/domain/AttendCourse.java
@@ -0,0 +1,49 @@
+package com.tms.sportlight.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class AttendCourse {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "attend_course_id")
+  private Integer id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "course_schedule_id")
+  private CourseSchedule courseSchedule;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_coupon_id")
+  private UserCoupon userCoupon;
+  private int participantNum;
+  private double finalAmount;
+  private double totalAmount;
+  private LocalDateTime requestDate;
+  private LocalDateTime completeDate;
+  private double paymentFee;
+  private int status;
+}
