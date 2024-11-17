dc618d9ed1f73c91608aebab2ed152804f28c360
Park Taehwan <xoghks211@naver.com>
2024-11-17 15:24:27 +0900

    ✨ FileRepository 메서드 추가
    프로필을 받아오기 위해 File 관련 Repo, Service에 메서드를 추가했습니다.

--- src/main/java/com/tms/sportlight/repository/JpaFileRepository.java
+++ src/main/java/com/tms/sportlight/repository/JpaFileRepository.java
@@ -2,7 +2,10 @@ package com.tms.sportlight.repository;
 
 import com.tms.sportlight.domain.FileType;
 import com.tms.sportlight.domain.UploadFile;
+import java.util.Optional;
+import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,10 @@ public interface JpaFileRepository extends JpaRepository<UploadFile, Integer> {
 
     List<UploadFile> findByTypeAndIdentifier(FileType fileType, int fileIdentifier);
 
+    @Query(value = "SELECT * FROM upload_file "
+        + "         WHERE file_type = ?1 AND file_identifier = ?2 AND deleted = 0 "
+        + "         ORDER BY reg_date DESC LIMIT 1", nativeQuery = true)
+    Optional<UploadFile> findRecentFile(FileType fileType, int fileIdentifier);
+
 }
+
