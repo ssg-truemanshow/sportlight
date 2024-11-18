90b3d73f5b18982905673a6861a22ad3829aa76b
Park Taehwan <xoghks211@naver.com>
2024-11-18 11:37:03 +0900

    ✨ 회원 엔티티, 서비스 및 컨트롤러 생성
    사용자의 요청을 처리하기 위한 컨트롤러 및 서비스를 생성하였습니다.

--- src/main/java/com/tms/sportlight/domain/User.java
+++ src/main/java/com/tms/sportlight/domain/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,42 +39,53 @@ public class User {
     private List<UserRole> roles = new ArrayList<>();
 
     private String userNickname;
-
     private String userIntroduce;
-
     private String userName;
-
     private String userGender;
-
     private String userBirth;
-
     private String userPhone;
-
     private Boolean termsAgreement;
-
     private Boolean marketingAgreement;
-
     private Boolean personalAgreement;
-
     private LocalDateTime regDate;
-
     private LocalDateTime modDate;
-
     private LocalDateTime delDate;
-
-    @Column(name = "user_state")
     private Boolean isDeleted;
-
-    @Column(name = "social_login")
     private String joinMethod;
 
     public void addRole(UserRole role) {
         if (roles == null) {
             roles = new ArrayList<>();
         } else if (roles.contains(role)) {
-            return; // 이미 존재하는 역할이므로 추가하지 않음
+            return;
+        } else {
+            roles = new ArrayList<>(roles);
         }
         roles.add(role);
+    }
+
+    @PreUpdate
+    public void userModTime() {
+        this.modDate = LocalDateTime.now();
+    }
+
+    public void updatePassword(String loginPwd) {
+        this.loginPwd = loginPwd;
+    }
+
+    public void update(String userNickname, String userIntroduce,Boolean marketingAgreement, Boolean personalAgreement) {
+        if (userNickname != null) {
+            this.userNickname = userNickname;
+        }
+        if (userIntroduce != null) {
+            this.userIntroduce = userIntroduce;
+        }
+        if (marketingAgreement != null) {
+            this.marketingAgreement = marketingAgreement;
+        }
+        if (personalAgreement != null) {
+            this.personalAgreement = personalAgreement;
+        }
     }
 
 
---
+++ src/main/java/com/tms/sportlight/service/UserService.java
@@ -0,0 +1,96 @@
+package com.tms.sportlight.service;
+
+import com.tms.sportlight.domain.Course;
+import com.tms.sportlight.domain.Review;
+import com.tms.sportlight.domain.User;
+import com.tms.sportlight.dto.ReviewDTO;
+import com.tms.sportlight.dto.UserDTO;
+import com.tms.sportlight.dto.UserUpdateDTO;
+import com.tms.sportlight.exception.BizException;
+import com.tms.sportlight.exception.ErrorCode;
+import com.tms.sportlight.repository.CourseRepository;
+import com.tms.sportlight.repository.ReviewRepository;
+import com.tms.sportlight.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final FileService fileService;
+    private final ReviewRepository reviewRepository;
+    private final CourseRepository courseRepository;
+
+    public UserDTO getProfile(User user) {
+        String userImage = fileService.getUserIconFile(Math.toIntExact(user.getId()));
+
+        return UserDTO.builder()
+            .userImage(userImage)
+            .loginId(user.getLoginId())
+            .userNickname(user.getUserNickname())
+            .userIntroduce(user.getUserIntroduce())
+            .userName(user.getUserName())
+            .userPhone(user.getUserPhone())
+            .marketingAgreement(user.getMarketingAgreement())
+            .personalAgreement(user.getPersonalAgreement())
+            .build();
+    }
+
+    @Transactional
+    public void updateProfile(User user, UserUpdateDTO userUpdateDTO) {
+
+        boolean isExist = userRepository.existsByUserNickname(userUpdateDTO.getUserNickname());
+
+        if (isExist) {
+            throw new BizException(ErrorCode.DUPLICATE_NICKNAME);
+        }
+
+        user.update(
+            userUpdateDTO.getUserNickname(),
+            userUpdateDTO.getUserIntroduce(),
+            userUpdateDTO.getMarketingAgreement(),
+            userUpdateDTO.getPersonalAgreement()
+        );
+
+    }
+
+    public List<ReviewDTO> getReviews(User user) {
+        List<Review> reviews = reviewRepository.findByUser(user);
+        return reviews.stream()
+            .map(ReviewDTO::fromEntity)
+            .toList();
+    } // return 수정?
+
+    @Transactional
+    public void writeReview(Integer id, User user, ReviewDTO reviewDTO) {
+        Course course = courseRepository.findById(id)
+            .orElseThrow(() -> new BizException(ErrorCode.NOT_FOUND_COURSE));
+
+        Review review = reviewDTO.toEntity(course, user);
+        reviewRepository.save(review);
+    }
+
+    @Transactional
+    public void modifyReview(Integer id, User user, String content, int rating) {
+        Review review = reviewRepository.findByIdAndUser(id, user)
+                .orElseThrow(() -> new BizException(ErrorCode.NOT_FOUND_REVIEW));
+
+        review.updateReview(content, rating);
+    }
+
+    @Transactional
+    public void deleteReview(Integer id, User user) {
+        Review review = reviewRepository.findByIdAndUser(id, user)
+            .orElseThrow(() -> new BizException(ErrorCode.NOT_FOUND_REVIEW));
+
+        reviewRepository.delete(review);
+    }
+
+
+}
