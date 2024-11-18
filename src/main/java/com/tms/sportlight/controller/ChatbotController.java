112adc487cba4c370d42917328a8ebb9012a2ff0
HonYong-Woo <1992wworld@naver.com>
2024-11-18 17:53:18 +0900

    Chatbot Controller 추가

---
+++ src/main/java/com/tms/sportlight/controller/ChatbotController.java
@@ -0,0 +1,140 @@
+package com.tms.sportlight.controller;
+
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/chatbot")
+public class ChatbotController {
+
+   @PostMapping("/open")
+    public String openChatbot() {
+        log.info("open chatbot");
+        return transmitMessage("", true);
+    }
+
+
+    @PostMapping("/send")
+    public String sendChatbot(@RequestBody String question) {
+        log.info("send chatbot");
+        log.info("question: " + question);
+        return transmitMessage(question, false);
+
+    }
+
+
+    /**
+     * 챗봇에게 메시지 전달
+     * @param question 질문
+     * @param isEvent 타입(true: open, false: send)
+     * @return
+     */
+    private String transmitMessage(String question, boolean isEvent) {
+      String chatbotMessage = "";
+
+      try {
+        String apiURL = "https://ssjj7muvrm.apigw.ntruss.com/custom/v1/16215/851ae207917e36a23409dd38553a9324c1f75314b002502cc08b034339716878";
+
+        URL url = new URL(apiURL);
+        String message = getReqMessage(question, isEvent);
+        log.info("message: " + message);
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", "RHhDQnBheExiSU93a2lwSm9oTVdGSHhuQnVGWVFyaUI=");
+
+        //요청
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.write(message.getBytes("UTF-8"));
+        wr.flush();
+        wr.close();
+
+        //응답
+        int responseCode = con.getResponseCode();
+
+        if(responseCode == 200) {
+          log.info("getResponseMessage : " + con.getResponseMessage());
+
+          BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+          String decodedString;
+
+          while ((decodedString = br.readLine()) != null) {
+            chatbotMessage = decodedString;
+          }
+          br.close();
+        }else {
+          chatbotMessage = con.getResponseMessage();
+        }
+
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+      log.info("chatbotMessage: " + chatbotMessage);
+      return chatbotMessage;
+    }
+
+
+
+    /**
+     * 요청 메시지 생성
+     * @param question 질문
+     * @param isEvent 타입(true: open, false: send)
+     * @return
+     */
+  private String getReqMessage(String question, boolean isEvent) {
+     String requestBody = "";
+     try {
+       JSONObject obj = new JSONObject();
+       long timestamp = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toEpochSecond();
+       log.info("timestamp: " + timestamp);
+
+       obj.put("version", "v1");
+       obj.put("userId", "사용자");
+       obj.put("timestamp", timestamp);
+
+       JSONObject content = new JSONObject();
+       content.put("type", "text");
+
+       JSONObject data = new JSONObject();
+       data.put("details", question);
+       content.put("data", data);
+
+       JSONArray contents = new JSONArray();
+       contents.add(content);
+
+       obj.put("content", contents);
+
+       if(isEvent) {
+         obj.put("event", "open");
+       } else {
+         obj.put("event", "send");
+       }
+
+       requestBody = obj.toString();
+     } catch (Exception e) {
+       e.printStackTrace();
+     }
+     return requestBody;
+  }
+
+}
