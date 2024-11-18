c13a07ba3d09243e7013e816e90c891cc081e49e
Park Taehwan <xoghks211@naver.com>
2024-11-18 11:39:15 +0900

    ✨ 회원정보 수정기능 구현
    회원정보 수정을 받는 DTO를 생성했습니다.

---
+++ src/main/java/com/tms/sportlight/dto/UserUpdateDTO.java
@@ -0,0 +1,34 @@
+package com.tms.sportlight.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO {
+
+    //프로필 이미지
+    private MultipartFile userImage;
+
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String userNickname;
+
+    private String userIntroduce;
+
+    private Boolean marketingAgreement;
+
+    private Boolean personalAgreement;
+
+    private Boolean isDeleted;
+
+}
