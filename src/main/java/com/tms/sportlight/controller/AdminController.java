d550db460eb3876e4f4c82e1d2d5615c29308389
Park Taehwan <xoghks211@naver.com>
2024-11-18 11:43:48 +0900

    ✨ 어드민 컨트롤러생성(테스트용)

---
+++ src/main/java/com/tms/sportlight/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.tms.sportlight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+        return "Admin Controller";
+    }
+
+}
