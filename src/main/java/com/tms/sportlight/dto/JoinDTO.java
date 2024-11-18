1844880484a2132865719320c3d3308b21928feb
Park Taehwan <xoghks211@naver.com>
2024-11-18 11:33:21 +0900

    ✨ 회원 가입 기능 구현
    회원가입을 위한 클래스 및 테스트 코드입니다.

--- src/main/java/com/tms/sportlight/dto/JoinDTO.java
+++ src/main/java/com/tms/sportlight/dto/JoinDTO.java
@@ -1,6 +1,8 @@
 package com.tms.sportlight.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,34 +12,30 @@ import lombok.Setter;
 @Builder
 public class JoinDTO {
 
+    @NotBlank
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String loginId;
 
+    @NotBlank
     private String loginPwd;
 
+    @NotBlank
     private String userNickname;
 
-    private String userIntroduce;
-
     private String userName;
-
     private String userGender;
-
     private String userBirth;
-
     private String userPhone;
 
-    private double userLatitude;
-
-    private double userLongitude;
-
+    @NotNull
     private Boolean termsAgreement;
 
     private Boolean marketingAgreement;
-
     private Boolean personalAgreement;
+    private String joinMethod;
 
-    private String socialLogin;
-
+    @NotNull
     private Boolean isDeleted;
+
+
 }
