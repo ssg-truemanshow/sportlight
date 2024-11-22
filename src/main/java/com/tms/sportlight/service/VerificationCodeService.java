377303bdcf74429afd2c99b9e2c12e76e5237ee3
Park Taehwan <xoghks211@naver.com>
2024-11-22 11:50:36 +0900

    🔧 String 타입 RedisTemplate 추가
    인증 코드를 설정하기 위한 설정 추가했습니다.

--- src/main/java/com/tms/sportlight/service/VerificationCodeService.java
+++ src/main/java/com/tms/sportlight/service/VerificationCodeService.java
@@ -2,44 +2,47 @@ package com.tms.sportlight.service;
 
 import com.tms.sportlight.security.util.VerificationCodeGenerator;
 import java.util.concurrent.TimeUnit;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class VerificationCodeService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, String> stringRedisTemplate;
     private static final int VERIFICATION_CODE_TTL = 180;
     private static final int LOGIN_ID_TTL = 300;
 
+    public VerificationCodeService(@Qualifier("stringRedisTemplate") RedisTemplate<String, String> stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     public String generateAndSaveCode(String loginId) {
         String code = VerificationCodeGenerator.generateCode();
-        redisTemplate.opsForValue().set(getVerificationCodeKey(loginId), code, VERIFICATION_CODE_TTL, TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(getVerificationCodeKey(loginId), code, VERIFICATION_CODE_TTL, TimeUnit.SECONDS);
         return code;
     }
 
     public boolean verifyCode(String loginId, String inputCode) {
         String key = getVerificationCodeKey(loginId);
-        String storedCode = redisTemplate.opsForValue().get(key);
+        String storedCode = stringRedisTemplate.opsForValue().get(key);
         return storedCode != null && storedCode.equals(inputCode);
     }
 
     public void disableCode(String loginId) {
-        redisTemplate.delete(getVerificationCodeKey(loginId));
+        stringRedisTemplate.delete(getVerificationCodeKey(loginId));
     }
 
     public void saveLoginId(String loginId) {
-        redisTemplate.opsForValue().set(getLoginIdKey(), loginId, LOGIN_ID_TTL, TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(getLoginIdKey(), loginId, LOGIN_ID_TTL, TimeUnit.SECONDS);
     }
 
     public String getLoginId() {
-        return redisTemplate.opsForValue().get(getLoginIdKey());
+        return stringRedisTemplate.opsForValue().get(getLoginIdKey());
     }
 
     public void deleteLoginId() {
-        redisTemplate.delete(getLoginIdKey());
+        stringRedisTemplate.delete(getLoginIdKey());
     }
 
     private String getVerificationCodeKey(String loginId) {
