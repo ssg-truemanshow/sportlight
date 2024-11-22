3aefcb3795ff8bd3a4f037594fe097f528fcc820
kinggora <jhha09@ajou.ac.kr>
2024-11-22 12:27:57 +0900

    ✨ 커뮤니티 상세 DTO 추가

---
+++ src/main/java/com/tms/sportlight/dto/CommunityDetailDTO.java
@@ -0,0 +1,20 @@
+package com.tms.sportlight.dto;
+
+import java.time.LocalDateTime;
+
+public class CommunityDetailDTO {
+
+    private int id;
+    private int categoryId;
+    private String categoryName;
+    private String title;
+    private String creatorName;
+    private int currentCapacity;
+    private int maxCapacity;
+    private String communityImg;
+    private String address;
+    private String detailAddress;
+    private double latitude;
+    private double longitude;
+    private LocalDateTime regDate;
+}
