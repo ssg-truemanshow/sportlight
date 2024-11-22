22f43bf9ea0d08cf5b71e7d8513c606c6bbe2c6f
kinggora <jhha09@ajou.ac.kr>
2024-11-22 11:02:12 +0900

    ✨ 유저 커뮤니티 리포지토리 추가

--- src/main/java/com/tms/sportlight/repository/JpaUserCommunityRepository.java
+++ src/main/java/com/tms/sportlight/repository/JpaUserCommunityRepository.java
@@ -2,6 +2,7 @@ package com.tms.sportlight.repository;
 
 import com.tms.sportlight.domain.CommunityParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,6 @@ public interface JpaUserCommunityRepository extends JpaRepository<CommunityParti
     void deleteByUserIdAndCommunityId(long userId, int communityId);
     void deleteByCommunityId(int communityId);
 
+    @Query("SELECT COUNT(cp.id) = 1 FROM CommunityParticipant cp WHERE cp.user.loginId=:username AND cp.community.id=:communityId")
+    boolean existsByUsernameAndCommunityId(String username, int communityId);
 }
