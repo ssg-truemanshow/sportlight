9542e4368104cb2dbdc90d955bddf1db2de535b2
kinggora <jhha09@ajou.ac.kr>
2024-11-22 11:07:12 +0900

    ✨ 커뮤니티 서비스: 리스트 조회 기능, 채팅방 입장/퇴장/강퇴 기능 추가

--- src/main/java/com/tms/sportlight/dto/CommunityListDTO.java
+++ src/main/java/com/tms/sportlight/dto/CommunityListDTO.java
@@ -13,33 +13,18 @@ import java.time.LocalDateTime;
 @ToString
 public class CommunityListDTO {
 
+    private int id;
     private int categoryId;
     private String categoryName;
     private String title;
     private String creatorName;
-    private String creatorImg;
     private int currentCapacity;
     private int maxCapacity;
     private String communityImg;
     private String address;
     private double latitude;
     private double longitude;
+    private double distance;
     private LocalDateTime regDate;
 
-//    public CommunityListDTO fromEntity(Community community) {
-//        return CommunityListDTO.builder()
-//                .categoryId(community.getCategory().getId())
-//                .categoryName(community.getCategory().getName())
-//                .title(community.getTitle())
-//                .creatorName(community.getUser().getUserNickname())
-//                .creatorImg("")
-//                .currentCapacity(community.getParticipants().size())
-//                .maxCapacity(community.getMaxCapacity())
-//                .communityImg("")
-//                .address(community.getAddress())
-//                .latitude(community.getLatitude())
-//                .longitude(community.getLongitude())
-//                .regDate(community.getRegDate())
-//                .build();
-//    }
 }
