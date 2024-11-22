40c1626f738149f101569fb8b99bc3a70e47afb4
Park Taehwan <xoghks211@naver.com>
2024-11-22 11:31:29 +0900

    🚧 리뷰기능 구현을 위한 관련 클래스 생성 및 작업 진행중

--- src/main/java/com/tms/sportlight/repository/JpaReviewRepository.java
+++ src/main/java/com/tms/sportlight/repository/JpaReviewRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
+public interface JpaReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findByUser(User user);
 
