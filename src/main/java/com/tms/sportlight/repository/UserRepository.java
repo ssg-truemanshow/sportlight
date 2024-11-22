e012abfbd2f42ad53365c70ba95550499f67cd32
Park Taehwan <xoghks211@naver.com>
2024-11-22 11:29:37 +0900

    🚧 회원 인증을 위한 메서드 설정 및 쿼리 작업 진행중

--- src/main/java/com/tms/sportlight/repository/UserRepository.java
+++ src/main/java/com/tms/sportlight/repository/UserRepository.java
@@ -5,6 +5,8 @@ import com.tms.sportlight.domain.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -13,5 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUserNickname(String userNickname);
 
     Optional<User> findByLoginId(String loginId);
+
+    @Query("SELECT u.loginId FROM User u WHERE u.userName = :userName AND u.userPhone = :userPhone AND u.isDeleted = false")
+    List<String> findAllLoginIds(@Param("userName") String userName, @Param("userPhone") String userPhone);
 
 }
