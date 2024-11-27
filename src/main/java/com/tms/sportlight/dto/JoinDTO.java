10155b3092505841b09de01395cae8c28a01a847
Park Taehwan <xoghks211@naver.com>
2024-11-27 14:43:24 +0900

    ♻️ 회원가입을 위한 구조 개선

--- src/main/java/com/tms/sportlight/dto/JoinDTO.java
+++ src/main/java/com/tms/sportlight/dto/JoinDTO.java
@@ -3,13 +3,17 @@ package com.tms.sportlight.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinDTO {
 
     @NotBlank
