723f753c4c3c3a4d2b8d8061d4779b29cdf98cb2
Park Taehwan <xoghks211@naver.com>
2024-12-04 18:29:56 +0900

    🔥 서버 테스트를 위한 엔드포인트 수정

--- src/main/java/com/tms/sportlight/controller/AuthController.java
+++ src/main/java/com/tms/sportlight/controller/AuthController.java
@@ -5,23 +5,28 @@ import com.tms.sportlight.domain.User;
 import com.tms.sportlight.dto.LoginIdFindRequestDTO;
 import com.tms.sportlight.dto.PasswordFindRequestDTO;
 import com.tms.sportlight.dto.PasswordResetDTO;
-import com.tms.sportlight.dto.PasswordResetVerifyDTO;
+import com.tms.sportlight.dto.PasswordVerifyRequestDTO;
 import com.tms.sportlight.dto.VerificationCodeDTO;
 import com.tms.sportlight.dto.common.DataResponse;
 import com.tms.sportlight.exception.BizException;
 import com.tms.sportlight.exception.ErrorCode;
-import com.tms.sportlight.repository.UserRepository;
+import com.tms.sportlight.security.CustomUserDetails;
+import com.tms.sportlight.security.oauth.dto.AdditionalSocialUserInfoDTO;
+import com.tms.sportlight.security.oauth.service.CustomOidcUserService;
 import com.tms.sportlight.service.AuthService;
 import com.tms.sportlight.util.JWTUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import java.time.Duration;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,13 +34,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
     private final JWTUtil jwtUtil;
-    private final RedisTemplate<String, String> stringRedisTemplate;
+    private final CustomOidcUserService customOidcUserService;
 
     /**
      * 이름과 휴대전화 번호로 아이디(이메일) 찾기
@@ -98,6 +103,31 @@ public class AuthController {
         response.addCookie(cookie);
 
         return DataResponse.of(newAccessToken);
+    }
+
+    @PostMapping("/verify-password")
+    public DataResponse<String> verifyPassword(@RequestBody PasswordVerifyRequestDTO request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userDetails.getUser();
+        authService.verifyCurrentPassword(user, request.getCurrentPassword());
+        return DataResponse.of("비밀번호가 확인되었습니다.");
+    }
+
+    @PostMapping("/change-password")
+    public DataResponse<String> changePassword(@RequestBody PasswordResetDTO request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userDetails.getUser();
+        authService.changePassword(user, request.getNewPwd());
+        return DataResponse.of("비밀번호가 성공적으로 변경되었습니다.");
+    }
+
+    @PatchMapping("/oauth/additional-info")
+    public DataResponse<String> updateAdditionalInfo(
+        @Valid @RequestBody AdditionalSocialUserInfoDTO additionalInfo,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        Long userId = userDetails.getUser().getId();
+        customOidcUserService.updateAdditionalInfo(userId, additionalInfo);
+
+        return DataResponse.of("추가 정보가 성공적으로 업데이트되었습니다.");
     }
 
 }
